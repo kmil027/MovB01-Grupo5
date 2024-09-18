fa15abb4d060c364f05e629f916a0b45f36b8235
Reinhard Ortiz <rtrortizm@poligran.edu.co>
2024-09-18 15:20:45 -0500

    Project init

---
+++ app/src/main/java/poli/MovB01Grupo5/proyectoportafoliopersonal/MainActivity.kt
@@ -0,0 +1,47 @@
+package poli.MovB01Grupo5.proyectoportafoliopersonal
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import poli.MovB01Grupo5.proyectoportafoliopersonal.ui.theme.ProyectoPortafolioPersonalTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProyectoPortafolioPersonalTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ProyectoPortafolioPersonalTheme {
+        Greeting("Android")
+    }
+}
