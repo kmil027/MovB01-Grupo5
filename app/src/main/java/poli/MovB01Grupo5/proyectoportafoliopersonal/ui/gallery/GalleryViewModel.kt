8ef44a8171193a9dbc681521f6f9e94c4583c0b1
Camilo Andrés Muñoz Gaitán <camunoz960@gmail.com>
2024-09-23 12:10:30 -0500

    Se configura la aplicación con menu lateral según el diseño

---
+++ app/src/main/java/poli/MovB01Grupo5/proyectoportafoliopersonal/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package poli.MovB01Grupo5.proyectoportafoliopersonal.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
