19bd93469fde3e77103d4dfcd875f8366c4edd3e
Camilo Andrés Muñoz <camunoz960@gmail.com>
2024-10-07 15:58:40 -0500

    Se implementa el navegador web, se actualizan permisos de la aplicación en Android - Camilo Muñoz

---
+++ app/src/main/java/poli/MovB01Grupo5/proyectoportafoliopersonal/ui/web/WebFragment.kt
@@ -0,0 +1,46 @@
+package poli.MovB01Grupo5.proyectoportafoliopersonal.ui.web
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.widget.Button
+import android.widget.EditText
+import poli.MovB01Grupo5.proyectoportafoliopersonal.R
+
+class WebFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = WebFragment()
+    }
+
+    private val viewModel: WebViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        val root = inflater.inflate(R.layout.fragment_web, container, false)
+        val urlView = root.findViewById<EditText>(R.id.urlToView)
+        val webBtn = root.findViewById<Button>(R.id.webBtn)
+        val webView = root.findViewById<WebView>(R.id.webView)
+
+        webBtn.setOnClickListener {
+            val url = urlView.text.toString()
+            webView.loadUrl(url)
+        }
+
+        return root
+
+    }
+}
---
+++ app/src/main/java/poli/MovB01Grupo5/proyectoportafoliopersonal/ui/web/WebViewModel.kt
@@ -0,0 +1,7 @@
+package poli.MovB01Grupo5.proyectoportafoliopersonal.ui.web
+
+import androidx.lifecycle.ViewModel
+
+class WebViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
