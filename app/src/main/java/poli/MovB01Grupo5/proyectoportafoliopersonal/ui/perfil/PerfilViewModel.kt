c17bf69580d232bf32442dc1b7c0db9175647817
Camilo Andrés Muñoz <camunoz960@gmail.com>
2024-10-08 22:55:25 -0500

    Se corrige el perfil profesional - Camilo Muñoz

--- app/src/main/java/poli/MovB01Grupo5/proyectoportafoliopersonal/ui/perfil/PerfilViewModel.kt
+++ app/src/main/java/poli/MovB01Grupo5/proyectoportafoliopersonal/ui/perfil/PerfilViewModel.kt
@@ -6,8 +6,4 @@ import androidx.lifecycle.ViewModel
 
 class PerfilViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is perfil Fragment"
-    }
-    val text: LiveData<String> = _text
 }
